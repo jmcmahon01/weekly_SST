d821c4e5248c88c1f3d1af1bb50d6bacabb437b1
jmcmahon01 <joseph.mcmahon2d9@gmail.com>
2024-05-15 12:18:43 -0700

    add test for LCMS 6, suite passes

--- __tests__/lcms.test.js
+++ __tests__/lcms.test.js
@@ -3,10 +3,12 @@ import { LCMS } from '../src/js/lcms.js';
 describe('LCMS', () => {
   let lcms2;
   let lcms5;
+  let lcms6;
 
   beforeEach(() => {
     lcms2 = new LCMS('LCMS 2');
     lcms5 = new LCMS('LCMS 5');
+    lcms6 = new LCMS('LCMS 6');
   });
 
   test('should correctly create a LCMS 2 object', () => {
@@ -14,6 +16,9 @@ describe('LCMS', () => {
   });
   test('should correctly create a LCMS 2 object', () => {
     expect(lcms5.instrument).toEqual('LCMS 5');
+  });
+  test('should correctly create a LCMS 6 object', () => {
+    expect(lcms6.instrument).toEqual('LCMS 6');
   });
 });
 
